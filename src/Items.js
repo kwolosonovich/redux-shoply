af6dc6d70a975c3ab96d12bfa4682b3c47008c35
kelly wolosonovich <k.wolosonovich@gmail.com>
2021-02-18 22:45:34 -0700

    applied add and remove buttons and basic display

--- src/Items.js
+++ src/Items.js
@@ -1,7 +1,19 @@
 import React, { useState } from "react";
 // import { Link } from "react-router-dom";
 import { useSelector, useDispatch } from "react-redux";
+import {
+  MDBBtn,
+  MDBCard,
+  MDBCardBody,
+  MDBCardImage,
+  MDBCardTitle,
+  MDBCardText,
+  MDBRow,
+  MDBCol,
+  MDBIcon,
+} from "mdbreact";
 import Item from "./Item"
+import './Items.css'
 
 function Items() {
   const { products } = useSelector((st) => st);
@@ -11,19 +23,45 @@ function Items() {
     (id) => (
       console.log(products[id].name),
       (
-        <div>
-          <h6>{products[id].name}</h6>
-          <h6>{products[id].price}</h6>
-          <h6>{products[id].description}</h6>
-          <img src={products[id].image_url}></img>
-        </div>
+        <MDBCol md="3">
+          <MDBCard>
+            <MDBCardImage
+              cascade
+              className="img-fluid"
+              overlay="white-light"
+              hover
+              src={products[id].image_url}
+            />
+            <MDBBtn
+              floating
+              tag="a"
+              className="ml-auto mr-4 lighten-3"
+              action
+            >Add</MDBBtn>
+            <MDBBtn
+              floating
+              tag="a"
+              className="ml-auto mr-4 lighten-3 mdb-coalor"
+              action
+            >Remove</MDBBtn>
+            <MDBCardBody cascade>
+              <MDBCardTitle>{products[id].name}</MDBCardTitle>
+              <hr />
+              <MDBCardText>
+                {products[id].description}
+                <br></br>
+                {products[id].price}
+              </MDBCardText>
+            </MDBCardBody>
+          </MDBCard>
+        </MDBCol>
       )
     )
   );
   return (
     <div>
       <h3>Items for Sale</h3>
-			{productList}
+      <MDBRow>{productList}</MDBRow>
     </div>
   );
 }
